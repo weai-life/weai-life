a51362ed793d9aa35f9298a304bbaed1c40fcd5e
stevenzg <guoyinfeng@live.com>
2023-12-03 15:59:07 +0800

    refactor: change post contentType to schema

---
+++ api/db/migrations/20231203075625_change_content_type_to_schema/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `contentType` on the `Post` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" DROP COLUMN "contentType",
+ADD COLUMN     "schema" TEXT;
