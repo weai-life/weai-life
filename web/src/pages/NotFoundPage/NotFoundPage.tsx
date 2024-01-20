be5f39c63189e18898351c4a0f959be4aae4fa37
stevenzg <guoyinfeng@live.com>
2024-01-20 18:26:24 +0800

    style: update not found page ui

--- web/src/pages/NotFoundPage/NotFoundPage.tsx
+++ web/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,44 +1,16 @@
+import { Button } from 'weai-ui'
+
+import { Link, routes } from '@redwoodjs/router'
+
 export default () => (
-  <main>
-    <style
-      dangerouslySetInnerHTML={{
-        __html: `
-              html, body {
-                margin: 0;
-              }
-              html * {
-                box-sizing: border-box;
-              }
-              main {
-                display: flex;
-                align-items: center;
-                font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif;
-                text-align: center;
-                background-color: #E2E8F0;
-                height: 100vh;
-              }
-              section {
-                background-color: white;
-                border-radius: 0.25rem;
-                width: 32rem;
-                padding: 1rem;
-                margin: 0 auto;
-                box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-              }
-              h1 {
-                font-size: 2rem;
-                margin: 0;
-                font-weight: 500;
-                line-height: 1;
-                color: #2D3748;
-              }
-            `,
-      }}
-    />
+  <main className="flex h-dvh w-full flex-col items-center justify-center">
     <section>
-      <h1>
-        <span>404 Page Not Found</span>
+      <h1 className="text-2xl font-semibold italic text-muted-foreground">
+        Page Not Found
       </h1>
     </section>
+    <Button variant="link" asChild className="text-md mt-4 underline">
+      <Link to={routes.tools()}>Back to home page</Link>
+    </Button>
   </main>
 )
